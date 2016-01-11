d5847c9abb328755087ab01093a0ffdfa6cf42f9
sohei <sohei@yamada>
2016-01-12 02:33:15 +0900

    test

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -8,7 +8,7 @@ return array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=localhost;dbname=test',
 			'username'   => 'root',
-			'password'   => '',
+			'password'   => 'kakihara',
 		),
 	),
 );
