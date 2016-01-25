46df6d1963b479a520c59426d47023106023b599
sohei <sohei@yamada>
2016-01-26 02:10:13 +0900

    create main modules

---
+++ fuel/app/views/m/dungeon/event/trap/create.php
@@ -0,0 +1,7 @@
+<h2>New <span class='muted'>M_dungeon_event_trap</span></h2>
+<br>
+
+<?php echo render('m/dungeon/event/trap/_form'); ?>
+
+
+<p><?php echo Html::anchor('m/dungeon/event/trap', 'Back'); ?></p>
